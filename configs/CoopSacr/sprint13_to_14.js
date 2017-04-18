be7803c995e55cff77c596d8a546a772019f539a
0Simba <b.dersoir@student.isartdigital.com>
2017-04-18 15:31:31 +0200

    Add sprint 13 to 14

---
+++ configs/CoopSacr/sprint13_to_14.js
@@ -0,0 +1,311 @@
+define([
+], function () {
+    'use strict';
+
+    return {
+        projectName    : 'Coopsacr',
+        reportIndex    : '13 to 14',
+        reportDay      : new Date(2017, 3, 18),
+        sprintDuration : 14,
+        calendar       : [
+            {
+                day:  new Date(2016, 9, 17),
+                name: 'Start'
+            },
+            {
+                day:  new Date(2016, 10, 3),
+                name: 'Concept'
+            },
+            {
+                day:  new Date(2016, 11, 16),
+                name: '3C'
+            },
+            {
+                day:  new Date(2017, 1, 23),
+                name: 'Alpha'
+            },
+            {
+                day:  new Date(2017, 3, 21),
+                name: 'Beta'
+            },
+            {
+                day:  new Date(2017, 4, 19),
+                name: 'Gold'
+            },
+            {
+                day:  new Date(2017, 5, 16),
+                name: 'Jury'
+            }
+        ],
+
+
+        burndown : {
+            totalPoints : 100,
+            states : [
+                {
+                    date       : new Date(2016, 9, 17),
+                    donePoints : 0
+                },
+                {
+                    date       : new Date(2016, 11, 6),
+                    donePoints : 14.2
+                },
+                {
+                    date       : new Date(2016, 11, 12),
+                    donePoints : 29.9
+                },
+                {
+                    date       : new Date(2016, 11, 16),
+                    donePoints : 32.0
+                },
+                {
+                    date       : new Date(2016, 11, 25),
+                    donePoints : 33.2
+                },
+                {
+                    date       : new Date(2017, 0, 1),
+                    donePoints : 33.2
+                },
+                {
+                    date       : new Date(2017, 0, 9),
+                    donePoints : 36.4
+                },
+                {
+                    date       : new Date(2017, 0, 15),
+                    donePoints : 29.3
+                },
+                {
+                    date       : new Date(2017, 0, 30),
+                    donePoints : 29.3
+                },
+                {
+                    date       : new Date(2017, 1, 6),
+                    donePoints : 30.7
+                },
+                {
+                    date       : new Date(2017, 1, 8),
+                    donePoints : 35
+                },
+                {
+                    date       : new Date(2017, 1, 13),
+                    donePoints : 37.9
+                },
+                {
+                    date       : new Date(2017, 1, 20),
+                    donePoints : 41.4
+                },
+                {
+                    date       : new Date(2017, 1, 27),
+                    donePoints : 42.9
+                },
+                {
+                    date       : new Date(2017, 2, 6),
+                    donePoints : 42.1
+                },
+                {
+                    date       : new Date(2017, 2, 13),
+                    donePoints : 42.1
+                },
+                {
+                    date       : new Date(2017, 2, 20),
+                    donePoints : 59.3
+                },
+                {
+                    date       : new Date(2017, 2, 27),
+                    donePoints : 60.7
+                },
+                {
+                    date       : new Date(2017, 3, 3),
+                    donePoints : 60.7
+                },
+                {
+                    date       : new Date(2017, 3, 10),
+                    donePoints : 60.7
+                },
+                {
+                    date       : new Date(2017, 3, 18),
+                    donePoints : 67.9
+                }
+            ]
+        },
+
+
+        main: {
+            bigNews : [
+                "Dog finished",
+                "Hero behaviors finished"
+            ],
+
+            indicators : {
+                global   : 'good',
+                morals   : 'good',
+                quality  : 'good',
+                planning : 'meh'
+            },
+
+            tasks : [
+                {
+                    name       : 'Hero',
+                    completion : 0.79,
+                    indicator  : 'good'
+                },
+                {
+                    name       : 'Enemies',
+                    completion : 0.84,
+                    indicator  : 'meh'
+                },
+                {
+                    name       : 'Level design',
+                    completion : 0.57,
+                    indicator  : 'meh'
+                },
+                {
+                    name       : 'Camera',
+                    completion : 0.56,
+                    indicator  : 'meh'
+                }
+            ],
+
+            risks : [
+                {
+                    name        : 'Git + unreal',
+                    probability : "High",
+                    gravity     : "High",
+                    solution    : 'Establish process. Verify everyone respect these process'
+                },
+                {
+                    name        : 'Graphic optimization',
+                    probability : "High",
+                    gravity     : "High",
+                    solution    : 'Pipeline anticipation'
+                },
+                {
+                    name        : 'Annoying sacrifices',
+                    probability : 'Low',
+                    gravity     : 'Very high',
+                    solution    : 'Lot of tests'
+                }
+            ]
+        },
+
+
+        jobs : [
+            {
+                title : 'Graphic Artist',
+                indicators: {
+                    global : 'meh'
+                },
+                tasks : [
+                    {
+                        name       : 'Hero animations',
+                        completion : 0.95,
+                        indicator  : 'good'
+                    },
+                    {
+                        name       : 'Dog',
+                        completion : 1,
+                        indicator  : 'good'
+                    },
+                    {
+                        name       : 'Humanoid texturing',
+                        completion : 0.2,
+                        indicator  : 'meh'
+                    },
+                    {
+                        name       : 'All environments',
+                        completion : 0.33,
+                        indicator  : 'bad'
+                    }
+                ],
+                decorationImage: './images/Coopsacr/art13to14.png'
+            },
+            {
+                title : 'Programmers',
+                indicators: {
+                    global : 'good'
+                },
+                tasks : [
+                    {
+                        name       : 'Hero behaviors',
+                        completion : 1,
+                        indicator  : 'meh'
+                    },
+                    {
+                        name       : 'Camera',
+                        completion : 0.60,
+                        indicator  : 'meh'
+                    },
+                    {
+                        name       : 'IA',
+                        completion : 0.80,
+                        indicator  : 'good'
+                    },
+                    {
+                        name       : 'Polish/integration',
+                        completion : 0.1,
+                        indicator  : 'bad'
+                    }
+                ],
+                decorationImage: './images/Coopsacr/prog6to7.png'
+            },
+            {
+                title : 'Game Designer',
+                indicators: {
+                    global : 'meh'
+                },
+                tasks : [
+                    {
+                        name       : 'Enemies spawns integration',
+                        completion : 0.80,
+                        indicator  : 'good'
+                    },
+                    {
+                        name       : 'Balancing',
+                        completion : 0,
+                        indicator  : 'bad'
+                    },
+                    {
+                        name       : 'FTUE',
+                        completion : 0.05,
+                        indicator  : 'bad'
+                    },
+                    {
+                        name       : 'Scenarisation',
+                        completion : 0.5,
+                        indicator  : 'meh'
+                    }
+                ],
+                decorationImage: './images/Coopsacr/gd8to9.png'
+            },
+            {
+                title : 'Sound Designers',
+                indicators: {
+                    global : 'meh'
+                },
+                tasks : [
+                    {
+                        name       : 'Wwise integration',
+                        completion : 1,
+                        indicator  : 'good'
+                    },
+                    {
+                        name       : 'Musics',
+                        completion : 0.65,
+                        indicator  : 'good'
+                    },
+                    {
+                        name       : 'Sounds effects',
+                        completion : 0.6,
+                        indicator  : 'bad'
+                    },
+                    {
+                        name       : 'Voice over',
+                        completion : 0.0,
+                        indicator  : '-'
+                    }
+                ],
+                decorationImage: './images/Coopsacr/ms/sprint6_to7.jpg'
+            }
+        ]
+    }
+});
---
+++ configs/FLY/sprint13_to_14.js
@@ -0,0 +1,308 @@
+define([
+], function () {
+    'use strict';
+
+    return {
+        projectName    : 'FLY',
+        reportIndex    : '13 to 14',
+        reportDay      : new Date(2017, 3, 18),
+        sprintDuration : 14,
+        calendar       : [
+            {
+                day:  new Date(2016, 9, 17),
+                name: 'Start'
+            },
+            {
+                day:  new Date(2016, 10, 3),
+                name: 'Concept'
+            },
+            {
+                day:  new Date(2016, 11, 16),
+                name: '3C'
+            },
+            {
+                day:  new Date(2017, 1, 23),
+                name: 'Alpha'
+            },
+            {
+                day:  new Date(2017, 3, 21),
+                name: 'Beta'
+            },
+            {
+                day:  new Date(2017, 4, 19),
+                name: 'Gold'
+            },
+            {
+                day:  new Date(2017, 5, 16),
+                name: 'Jury'
+            },
+        ],
+
+
+        burndown : {
+            totalPoints : 100,
+            states : [
+                {
+                    date       : new Date(2016, 9, 17),
+                    donePoints : 0
+                },
+                {
+                    date       : new Date(2016, 10, 28),
+                    donePoints : 6.6
+                },
+                {
+                    date       : new Date(2016, 11, 6),
+                    donePoints : 6.6
+                },
+                {
+                    date       : new Date(2016, 11, 12),
+                    donePoints : 6.6
+                },
+                {
+                    date       : new Date(2016, 11, 16),
+                    donePoints : 16.5
+                },
+                {
+                    date       : new Date(2016, 11, 25),
+                    donePoints : 16.5
+                },
+                {
+                    date       : new Date(2017, 0, 1),
+                    donePoints : 17.1
+                },
+                {
+                    date       : new Date(2017, 0, 9),
+                    donePoints : 25.2
+                },
+                {
+                    date       : new Date(2017, 0, 15),
+                    donePoints : 28.5
+                },
+                {
+                    date       : new Date(2017, 0, 24),
+                    donePoints : 25.7
+                },
+                {
+                    date       : new Date(2017, 0, 30),
+                    donePoints : 25.7
+                },
+                {
+                    date       : new Date(2017, 1, 6),
+                    donePoints : 31.4
+                },
+                {
+                    date       : new Date(2017, 1, 13),
+                    donePoints : 38.6
+                },
+                {
+                    date       : new Date(2017, 1, 20),
+                    donePoints : 41.40
+                },
+                {
+                    date       : new Date(2017, 1, 27),
+                    donePoints : 42.8
+                },
+                {
+                    date       : new Date(2017, 2, 6),
+                    donePoints : 42.8
+                },
+                {
+                    date       : new Date(2017, 2, 13),
+                    donePoints : 42.8
+                },
+                {
+                    date       : new Date(2017, 2, 20),
+                    donePoints : 52.9
+                },
+                {
+                    date       : new Date(2017, 2, 27),
+                    donePoints : 53.6
+                },
+                {
+                    date       : new Date(2017, 3, 3),
+                    donePoints : 53.6
+                },
+                {
+                    date       : new Date(2017, 3, 10),
+                    donePoints : 53.6
+                },
+                {
+                    date       : new Date(2017, 3, 18),
+                    donePoints : 53.6
+                }
+            ]
+        },
+
+
+        main: {
+            bigNews : [
+            ],
+
+            indicators : {
+                global   : 'meh',
+                morals   : 'meh',
+                quality  : 'meh',
+                planning : 'bad'
+            },
+
+            tasks : [
+                {
+                    name       : 'Boss',
+                    completion : 0.56,
+                    indicator  : 'bad'
+                },
+                {
+                    name       : 'Camera',
+                    completion : 0.78,
+                    indicator  : 'bad'
+                },
+                {
+                    name       : 'Boss main phases',
+                    completion : 0.4,
+                    indicator  : 'bad'
+                },
+                {
+                    name       : 'plane',
+                    completion : 0.69,
+                    indicator  : 'bad'
+                }
+            ],
+
+            risks : [
+                {
+                    name        : 'Plane and monster not well polished',
+                    probability : 'Very high',
+                    gravity     : 'Very high',
+                    solution    : 'Allow very high time on polish, minimize features quantities'
+                },
+                {
+                    name        : 'Boss size',
+                    probability : 'High',
+                    gravity     : 'Medium',
+                    solution    : 'Cut boss in multiple part'
+                }
+            ]
+        },
+
+
+        jobs : [
+            {
+                title : 'Graphic Artist',
+                indicators: {
+                    global : 'bad'
+                },
+                tasks : [
+                    {
+                        name       : 'Boss modules',
+                        completion : 0.56,
+                        indicator  : 'bad'
+                    },
+                    {
+                        name       : 'Plane & FX',
+                        completion : 0.5,
+                        indicator  : 'meh'
+                    },
+                    {
+                        name       : 'Tornado',
+                        completion : 0,
+                        indicator  : 'bad'
+                    },
+                    {
+                        name       : 'UI - menus',
+                        completion : 0.2,
+                        indicator  : 'meh'
+                    }
+                ],
+                decorationImage: './images/FLY/graph10.png'
+            },
+            {
+                title : 'Programmers',
+                indicators: {
+                    global : 'meh'
+                },
+                tasks : [
+                    {
+                        name       : 'Player controler',
+                        completion : 0.95,
+                        indicator  : 'good'
+                    },
+                    {
+                        name       : 'Camera\'s systems',
+                        completion : 0.9,
+                        indicator  : 'meh'
+                    },
+                    {
+                        name       : 'Boss behaviors',
+                        completion : 0.36,
+                        indicator  : 'bad'
+                    },
+                    {
+                        name       : 'Menus',
+                        completion : 0.46,
+                        indicator  : 'meh'
+                    }
+                ],
+                decorationImage: './images/FLY/3C/prog.png'
+            },
+            {
+                title : 'Game Designer',
+                indicators: {
+                    global : 'meh'
+                },
+                tasks : [
+                    {
+                        name       : 'Cinematics placeholder',
+                        completion : 0.2,
+                        indicator  : 'meh'
+                    },
+                    {
+                        name       : 'IA',
+                        completion : 0.1,
+                        indicator  : 'meh'
+                    },
+                    {
+                        name       : 'Plane settings & tests',
+                        completion : 0.8,
+                        indicator  : 'meh'
+                    },
+                    {
+                        name       : 'Camera settings & tests',
+                        completion : 0.5,
+                        indicator  : 'meh'
+                    }
+                ],
+                decorationImage: './images/FLY/gd10.png'
+            },
+            {
+                title : 'Sound Designers',
+                indicators: {
+                    global : 'good'
+                },
+                tasks : [
+                    {
+                        name       : 'Wwise integration',
+                        completion : 0.45,
+                        indicator  : 'good'
+                    },
+                    {
+                        name       : 'Musics',
+                        completion : 0.6,
+                        indicator  : 'good'
+                    },
+                    {
+                        name       : 'Sounds effects',
+                        completion : 0.5,
+                        indicator  : 'good'
+                    },
+                    {
+                        name       : 'Mix',
+                        completion : 0.4,
+                        indicator  : 'good'
+                    }
+                ],
+                decorationImage: './images/placeholders/sounddesign.jpg'
+            }
+        ]
+    }
+});
+
